521af6b15094e2afd10e33c2c14b30fd289f9b96
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2013-05-26 14:30:27 +0300

    finalize form validation

--- examples/form-validation.js
+++ examples/form-validation.js
@@ -8,12 +8,16 @@ angular.
                 return it && it.length > num;
             }
         }
-        var passwordsEqual =
-            $scope
+        var identity = function(it) {
+            return it;
+        }
+        var passwordInput = $scope
                 .$watchAsStream('password')
-                .filter(atLeast(2))
+                .toProperty('')
+
+        var passwordsEqual = passwordInput
                 .combine(
-                    $scope.$watchAsStream('passwordConfirm').filter(atLeast(2)),
+                    $scope.$watchAsStream('passwordConfirm'),
                     function(pass, confirm) {
                         return {
                             password: pass,
@@ -24,15 +28,47 @@ angular.
                 .map(function(it) {
                     return it.password === it.confirm
                 })
+                .toProperty()
 
         var usernameInput = $scope
-            .$watchAsStream('username')
+                .$watchAsStream('username')
+                .toProperty()
+
+        var passwordIsValid = passwordInput
+                .map(function(it) { return it && it.length >= 5 })
 
         var usernameIsFree = usernameInput
-            .filter(function(it) { return it.length > 2 })
-            .flatMapLatest(function(it) { return Bacon.later(2000, it === "user"); })
-            .merge(usernameInput.map(false))
-            .toProperty()
+                .filter(function(it) { return it.length > 2 })
+                .flatMapLatest(function(it) { return Bacon.later(2000, it === "user"); })
+                .merge(usernameInput.map(false))
+                .toProperty(false)
 
-        usernameIsFree.and(passwordsEqual).digest($scope, 'formValid')
+        usernameIsFree
+            .and(passwordIsValid)
+            .and(passwordsEqual)
+            .digest($scope, 'formValid')
+
+        usernameIsFree
+            .changes()
+            .filter(identity)
+            .map(Bacon.constant("free"))
+            .merge(usernameIsFree.changes().not().filter(identity).map(Bacon.constant("taken")))
+            .merge(usernameInput.map(Bacon.constant("loading")))
+            .merge(
+                usernameInput
+                    .filter(function(it) { return it.length < 3 })
+                    .map(Bacon.constant("empty"))
+                )
+            .digest($scope, 'usernameState')
+
+        passwordIsValid
+            .changes()
+            .not()
+            .digest($scope, 'passwordInvalid')
+
+        passwordsEqual
+            .changes()
+            .not()
+            .digest($scope, 'passwordsDontMatch')
+
     })
